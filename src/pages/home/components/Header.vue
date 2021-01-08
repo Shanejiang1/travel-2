75040eae8f10e33c577efe06ffb414f9849f0ddf
Shanejiang1 <jiangshanghang@gmail.com>
2021-01-08 18:22:54 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,13 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
+
+    <router-link to="/city">
     <div class="header-right">
       {{this.city}}
       <span class="iconfont arrow-icon">&#xe64a;</span>
     </div>
+    </router-link>
   </div>
 </template>
 
@@ -52,6 +55,7 @@ export default {
     width: 1.24rem;
     float: right;
     text-align: center;
+    color: #ffffff;
   }
   .arrow-icon {
     margin-left: -.04rem;
