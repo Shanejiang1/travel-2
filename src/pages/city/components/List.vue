3e75c92be4876f23f6d491dfeaaeb36f0e429769
Shanejiang1 <jiangshanghang@gmail.com>
2021-01-10 17:33:41 +0800

    修复程序 bug

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -80,7 +80,9 @@ export default {
     }
   },
   mounted(){
-    this.scroll = new Bscroll(this.$refs.wrapper)
+    this.scroll = new Bscroll(this.$refs.wrapper, {
+      click:true
+    })
   },
 }
 </script>
