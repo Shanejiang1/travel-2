7a00c554059942c7bd77a9f39e9bd46b8016c8bd
Shanejiang1 <jiangshanghang@gmail.com>
2021-01-11 20:23:03 +0800

    修复 bug

--- src/pages/detail/components/List.vue
+++ src/pages/detail/components/List.vue
@@ -39,7 +39,7 @@ export default {
     width: .36rem;
     height: .36rem;
     background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat;
-    margin-right: 1rem;
+    margin-right: .1rem;
     background-size: .4rem 3rem;
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -10,9 +10,5 @@ module.exports = {
         }
       }
     }
-  },
-  // chainWebpack: (config) => {
-  //   config.resolve.alias
-  //     .set('common', path.join(__dirname, './src/common/'))
-  // }
+  }
 }
